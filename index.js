a16d6c53be2be6343bbf13a56996fcd6c86b5ed6
Terrie Ann <malobaterryann@gmail.com>
2024-11-25 14:19:28 -0800

    math functions

---
+++ index.js
@@ -0,0 +1,49 @@
+//add function
+function add(){
+
+}
+//subtract function
+function subtract(){
+
+}
+//multiply function
+function multiply(){
+
+}
+//divide function
+function divide(){
+
+}
+//add a+b
+function add(a, b){
+    return a+b;
+}
+//subtract a-b
+function subtract(a, b){
+    return a-b;
+}
+//multiply a*b
+function multiply(a,b){
+    return a*b;
+}
+//function a/b
+function divide(a,b){
+    return a/b;
+}
+//increment
+function increment(n){
+    return ++n;
+}
+//decrement
+function decrement(n){
+    return --n;
+}
+//parseInt
+function makeInt(n){
+    return parseInt(n, 10);
+
+}
+//parseFloat
+function preserveDecimal(n){
+    return parseFloat(n);
+}
